e9698b032d75094972dddcf3d80fa7962ba7d2f8
Kamil Janusz <kamiljanusztc@gmail.com>
2022-07-26 10:15:29 +0200

    Change url

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var apiRoot = 'https://tasks-kj.herokuapp.com/v1/';
+    var apiRoot = 'https://tasks-kj.herokuapp.com/v1/tasks';
     var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     var tasksContainer = $('[data-tasks-container]');
 
